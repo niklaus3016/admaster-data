548780b1a84e756c4e3c72fd92b59d4f2c1e07c6
niklaus3016 <niklaus3016@users.noreply.github.com>
2026-03-06 19:02:43 +0000

    修复插件注册：在super.onCreate之前注册BaiduAdPlugin

--- android/app/src/main/java/com/gaoqianleme/app/MainActivity.java
+++ android/app/src/main/java/com/gaoqianleme/app/MainActivity.java
@@ -2,15 +2,13 @@ package com.gaoqianleme.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
         // 注册百度广告插件
         registerPlugin(BaiduAdPlugin.class);
+        
+        super.onCreate(savedInstanceState);
     }
 }
